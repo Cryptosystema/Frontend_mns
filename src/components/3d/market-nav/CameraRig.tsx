fdebc478ab8ba944bedf6f4a77c40e270b5f94d6
Cryptosystema <gmfingeo@gmail.com>
2026-02-27 11:44:14 +0000

    fix: camera higher angle to see peaks

--- src/components/3d/market-nav/CameraRig.tsx
+++ src/components/3d/market-nav/CameraRig.tsx
@@ -4,8 +4,8 @@ import { useThree } from "@react-three/fiber"
 export function CameraRig() {
   const { camera } = useThree()
   useEffect(() => {
-    camera.position.set(0, 18, 28)
-    camera.lookAt(0, -2, -8)
+    camera.position.set(0, 30, 25)
+    camera.lookAt(0, 0, -5)
   }, [camera])
   return null
 }
